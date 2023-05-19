8e566ad4f42adcbcb94bba462fd41671136554f9
Mitchel Dickerson <mgdicker@uci.edu>
2023-05-19 14:51:22 -0700

    Removed hw6 files and updated CMakeLists.txt.

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "bst.h"
-
-int main()
-{
-    measure_BSTs("src/shortrandom.txt");
-    // measure_BSTs("src/random.txt");
-    measure_BSTs("src/shortsorted.txt");
-    // measure_BSTs("src/sorted.txt");
-}
